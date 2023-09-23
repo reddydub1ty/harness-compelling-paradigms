2f5211154f9aaf6f41301e9f2446c8b6451da379
reddydub1ty <139818835+reddydub1ty@users.noreply.github.com>
2023-09-23 23:43:49 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,41 @@
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+if __name__ == "__main__":
+
+    for item in data:
+    return data
+    main()
+
+
+import random
+def main():            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+        for (int item : data) {
+        }
+        }
+}
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        int[] data = new int[10];
+
+    }    }
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
+    });
+    const data = [];
+    data.forEach(item => {
+function main() {
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+    return data;
+
+}
+
