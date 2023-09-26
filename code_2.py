8e08776370ace8133dd7821576c68f9e8c18fcec
reddydub1ty <139818835+reddydub1ty@users.noreply.github.com>
2023-09-26 07:13:05 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,57 @@
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+}
+
+        }
+        }
+    }
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
+}
+
+
+function main() {
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+    const data = [];
+    data.forEach(item => {
+function generateRandomData() {
+}
+
+    });
+    return data;
+    for (let i = 0; i < 10; i++) {}
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    return data;
+    const data = generateRandomData();
+
+        console.log("Random Number: " + item);
+    const data = [];
+function generateRandomData() {
+    });
+    data.forEach(item => {
+    }
+    for (let i = 0; i < 10; i++) {
+
+}
+function main() {    return data
+    data = generate_random_data()
+if __name__ == "__main__":
+
+
+import random
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+def generate_random_data():
+    for item in data:
+
+        print(f"Random Number: {item}")
