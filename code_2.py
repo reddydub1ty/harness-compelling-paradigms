b42f1b06d35ca037637a2028ec16138ace99f506
reddydub1ty <139818835+reddydub1ty@users.noreply.github.com>
2023-10-07 20:30:44 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,56 +1,38 @@
+        }
 
-
-def generate_random_data():
-    for item in data:
-    main()
-
-        print(f"Random Number: {item}")
-    return data
-    data = generate_random_data()
-if __name__ == "__main__":
-
-    data = [random.randint(1, 100) for _ in range(10)]
-def main():
-import random}
-    const data = [];
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+}
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
     }
-main();
-    const data = generateRandomData();
-function generateRandomData() {
+public class RandomDataGenerator {
+        int[] data = new int[10];
 
+        for (int item : data) {        int[] data = new int[10];
+public class RandomDataGenerator {
 
-    return data;
+        }
+        for (int i = 0; i < 10; i++) {
+    }
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
 
-    for (let i = 0; i < 10; i++) {
-        data.push(Math.floor(Math.random() * 100) + 1);
-    data.forEach(item => {
-function main() {
-        console.log("Random Number: " + item);
-    });
-}    data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
+        for (int item : data) {
+        }    data = generate_random_data()
 
-
-
-
-    main()
     for item in data:
-        print(f"Random Number: {item}")
     return data
+    main()
+
+if __name__ == "__main__":
+import random
 def main():
-    data = generate_random_data()
+
 def generate_random_data():
-import random        print(f"Random Number: {item}")
-
-
-    for item in data:
 
     data = [random.randint(1, 100) for _ in range(10)]
-
-import random
-    main()
-    data = generate_random_data()
-def generate_random_data():
-def main():
-    return data
-if __name__ == "__main__":
+        print(f"Random Number: {item}")
